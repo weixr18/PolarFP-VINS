3a397674a3a0acffb50809ab62067f3a591a6fd3
weixr18 <weixr0605@sina.com>
2026-04-02 19:57:18 +0800

    fix cmake/opencv version bugs, compilable

--- vins_estimator/src/initial/initial_sfm.cpp
+++ vins_estimator/src/initial/initial_sfm.cpp
@@ -242,7 +242,7 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 */
 	//full BA
 	ceres::Problem problem;
-	ceres::LocalParameterization* local_parameterization = new ceres::QuaternionParameterization();
+	ceres::Manifold* local_parameterization = new ceres::QuaternionManifold();
 	//cout << " begin full BA " << endl;
 	for (int i = 0; i < frame_num; i++)
 	{
